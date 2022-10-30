0ee0503b32db6ba5913a7ca7c08c7b8a985aec8f
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-10-30 10:38:11 -0400

    refactor(voicenote): add messages for not audio and receiving request

--- pages/api/voicenote.ts
+++ pages/api/voicenote.ts
@@ -6,16 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  console.log('Received', req.body);
-  const userNumber = req.body.sender.id;
-  const typeOfMedia = req.body.message.attachments[0].type;
-
-  if (typeOfMedia !== 'audio') {
-    console.log('ERROR -->> not audio');
-    res.status(400).json({ error: 'Not audio' });
-  }
-
-  const audio = req.body.message.attachments[0].url;
 
   const {
     BANANA_API_KEY: bananaApiKey,
@@ -36,6 +26,53 @@ export default async function handler(
     res.status(500).json({ error: 'No env variables' });
   }
 
+  console.log('Received', req.body);
+  const userNumber = req.body.sender.id;
+  const typeOfMedia = req.body.message.attachments[0].type || 'not audio';
+
+  if (typeOfMedia !== 'audio') {
+
+    console.log('ERROR -->> not audio');
+
+    await axios.post(
+      `https://api.connectly.ai/v1/businesses/${{ businessId }}/send/messages`,
+      {
+        sender: {
+          id: aiNUmber,
+          channelType: 'whatsapp'
+        },
+        recipient: {
+          id: userNumber,
+          channelType: 'whatsapp'
+        },
+        message: {
+          text: 'You can only send audio files'
+        }
+      }
+    );
+
+    res.status(400).json({ error: 'Not audio' });
+  }
+
+  await axios.post(
+    `https://api.connectly.ai/v1/businesses/${{ businessId }}/send/messages`,
+    {
+      sender: {
+        id: aiNUmber,
+        channelType: 'whatsapp'
+      },
+      recipient: {
+        id: userNumber,
+        channelType: 'whatsapp'
+      },
+      message: {
+        text: 'Your request has been received. Please give us a a minute or two to process your request.'
+      }
+    }
+  );
+
+  const audio = req.body.message.attachments[0].url;
+
   let result: any = await axios
     .get(audio, {
       responseType: 'arraybuffer'
@@ -51,7 +88,7 @@ export default async function handler(
 
   console.log('Output =>', output);
 
-  const connectlyReponse = await axios.post(
+  const connectlyResponse = await axios.post(
     `https://api.connectly.ai/v1/businesses/${{ businessId }}/send/messages`,
     {
       sender: {
@@ -67,5 +104,5 @@ export default async function handler(
       }
     }
   );
-  res.status(200).json({ message: 'output', response: connectlyReponse });
+  res.status(200).json({ message: 'output', response: connectlyResponse });
 }
